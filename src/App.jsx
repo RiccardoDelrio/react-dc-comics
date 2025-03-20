4cd0954521efb2870240c2c64977a017d18fc4b7
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-20 15:33:40 +0100

    refactor: remove App.css and enhance styling in index.css for Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 /* import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg' */
-import './App.css'
 import Headers from './components/Header'
 import Footers from './components/Footer'
 import Main from './components/Main'
