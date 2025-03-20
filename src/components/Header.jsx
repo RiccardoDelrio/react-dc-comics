d5aeef7c8dfd43514cb4cfd4330b890513f9bf9c
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-20 14:45:12 +0100

    feat: update index.html title and add new images; enhance Header component with navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,31 @@
 export default function Headers() {
     return (
-        <h1>Header</h1>
+        <header>
+            <div className="navbar">
+                <div className="left">
+                    <div className="logo">
+                        <img src="./img/dc-logo.png" alt="" />
+                    </div>
+                </div>
+                <div className="right">
+                    <ul>
+                        <li><a href="#">CHARATERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+
+
+                    </ul>
+
+                </div>
+            </div>
+        </header>
+
     )
 }
