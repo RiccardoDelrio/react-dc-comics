1160dcfd9fa28644e4149d640b454b411fa867c1
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-21 15:49:33 +0100

    feat: update Main component to dynamically render Card components from comics data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,9 @@
 import Card from './card'
 import comics from '/data/comics.js'
 export default function Main() {
+
+
+
     return (
 
 
@@ -10,8 +13,10 @@ export default function Main() {
                     Current series
                 </div>
 
-                <Card title="Action Comics" src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY" />
-
+                {comics.map((comic) => (
+                    <Card src={comic.thumb} title={comic.series} />
+                )
+                )}
 
 
 
